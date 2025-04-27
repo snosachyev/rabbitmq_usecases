b92dcf6887c36ab703c709f55cdc790678ae9e56
Сергей Носачёв <s.nosachev@fkk.team>
2025-04-27 21:15:39 +0300

    added writer fanout

--- consumer/main.py
+++ consumer/main.py
@@ -3,12 +3,14 @@ import os
 import sys
 
 from consumer.write_csv_work_queues_use_case import WriteToCSVWorkQueuesCase
+from consumer.write_fanout_message_work_queues_use_case import WriteFanoutMessageWorkQueuesCase
 from consumer.write_message_work_queues_use_case import WriteMessageWorkQueuesCase
 
 
 def execute():
     WriteMessageWorkQueuesCase().execute()
     WriteToCSVWorkQueuesCase().execute()
+    WriteFanoutMessageWorkQueuesCase().execute()
 
 if __name__ == "__main__":
     ch = logging.StreamHandler(sys.stdout)
@@ -23,6 +25,7 @@ if __name__ == "__main__":
     logging.info(f"Starting consumer...")
     consumers = {
         'WriteMessageWorkQueuesCase': WriteMessageWorkQueuesCase,
-        'WriteToCSVWorkQueuesCase': WriteToCSVWorkQueuesCase
+        'WriteToCSVWorkQueuesCase': WriteToCSVWorkQueuesCase,
+        'WriteFanoutMessageWorkQueuesCase': WriteFanoutMessageWorkQueuesCase
     }
     sys.exit(consumers.get(os.environ['CONSUMER'], 'WriteMessageWorkQueuesCase')().execute())
