53509d1807d2c661553c43f0c4e73ba01afa5b26
Сергей Носачёв <s.nosachev@fkk.team>
2025-04-27 21:12:36 +0300

    add consumer entry

---
+++ consumer/main.py
@@ -0,0 +1,28 @@
+import logging
+import os
+import sys
+
+from consumer.write_csv_work_queues_use_case import WriteToCSVWorkQueuesCase
+from consumer.write_message_work_queues_use_case import WriteMessageWorkQueuesCase
+
+
+def execute():
+    WriteMessageWorkQueuesCase().execute()
+    WriteToCSVWorkQueuesCase().execute()
+
+if __name__ == "__main__":
+    ch = logging.StreamHandler(sys.stdout)
+
+    logging.basicConfig(
+        level=20,
+        format="%(asctime)s [%(levelname)s] %(funcName)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[ch]
+    )
+
+    logging.info(f"Starting consumer...")
+    consumers = {
+        'WriteMessageWorkQueuesCase': WriteMessageWorkQueuesCase,
+        'WriteToCSVWorkQueuesCase': WriteToCSVWorkQueuesCase
+    }
+    sys.exit(consumers.get(os.environ['CONSUMER'], 'WriteMessageWorkQueuesCase')().execute())
